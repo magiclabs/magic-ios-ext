7e1f5c020c29b535baa5ec511fd26643ff4c0716
nick <nick@floornfts.io>
2023-03-03 14:28:51 -0500

    fix

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             targets: ["MagicExt-OIDC"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/magiclabs/magic-ios.git", from:"4.0.0"),
+        .package(url: "https://github.com/magiclabs/magic-ios.git", from:"7.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
